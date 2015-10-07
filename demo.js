e520efaad0b7848869926d0a9cf1113371c07b95
Grant Miner <llambda@users.noreply.github.com>
2015-10-06 23:32:53 -0500

    Reindent

--- demo.js
+++ demo.js
@@ -3,29 +3,29 @@ var modal = require('./modal/modal.js');
 
 //top level
 var demo = {
-	view: function(ctrl) {
-		return [
-			m("button[type=button]", {
-                onclick: function () {
+    view: function(ctrl) {
+        return [
+            m("button[type=button]", {
+                onclick: function() {
                     modal.show();
                 }
             }, "Click to show modal"),
-			m.component(modal, {
+            m.component(modal, {
                 view: inner.view,
                 class: 'modal-animation-8'
             })
-		]
-	}
+        ]
+    }
 }
 
 //to be rendered in the modal
 var inner = {
-	controller: function() {},
-	view: function() {
-		return [
-			m("h1", "Hello world")
-		]
-	}
+    controller: function() {},
+    view: function() {
+        return [
+            m("h1", "Hello world")
+        ]
+    }
 }
 
 m.module(document.body, demo)
--- modal/modal.js
+++ modal/modal.js
@@ -1,57 +1,57 @@
 var m = require('mithril');
 var visible = m.prop(false);
 
-module.exports.show = function () {
-	visible(true);
+module.exports.show = function() {
+    visible(true);
 }
 
-var hide = function () {
-	visible(false);
+var hide = function() {
+    visible(false);
 }
 module.exports.hide = hide;
 
 module.exports.controller = function(args, extras) {
-	var ctrl = this;
+    var ctrl = this;
 
-	ctrl.onunload = function () {
-		document.body.removeEventListener('keyup', handleKey)
-	}
+    ctrl.onunload = function() {
+        document.body.removeEventListener('keyup', handleKey)
+    }
 
-	ctrl.config = function(element, isInitialized, context) {
-		if (!isInitialized) {
-			var handleKey = function(e) {
-				if (e.keyCode == 27) {
-					visible(false);
-					m.redraw()
-				}
-			}
+    ctrl.config = function(element, isInitialized, context) {
+        if (!isInitialized) {
+            var handleKey = function(e) {
+                if (e.keyCode == 27) {
+                    visible(false);
+                    m.redraw()
+                }
+            }
 
-			document.body.addEventListener('keyup', handleKey)
-		}
-	}
+            document.body.addEventListener('keyup', handleKey)
+        }
+    }
 }
 
 module.exports.view = function(ctrl, args, extras) {
-	args = args || {}
-	return m('div', [
-		m(".modal", {
-			class: [
-				visible() ? "modal-visible" : "",
-				args.class
-			].join(" "),
-			onclick: hide,
-			config: ctrl.config
-		}, [
-			m(".modal-dialog", [
-				m("a.modal-close", {
-					onclick: hide,
-					style: {
-						cursor: 'pointer'
-					}
-				}, "X"),
-				args.view ? args.view() : ''
-			])
-		]),
-		m(".modal-overlay")
-	])
+    args = args || {}
+    return m('div', [
+        m(".modal", {
+            class: [
+                visible() ? "modal-visible" : "",
+                args.class
+            ].join(" "),
+            onclick: hide,
+            config: ctrl.config
+        }, [
+            m(".modal-dialog", [
+                m("a.modal-close", {
+                    onclick: hide,
+                    style: {
+                        cursor: 'pointer'
+                    }
+                }, "X"),
+                args.view ? args.view() : ''
+            ])
+        ]),
+        m(".modal-overlay")
+    ])
 }
