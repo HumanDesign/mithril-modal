9392d6485c420df593c73eafbc05779fbb2b054c
Grant Miner <xxgsoftware@gmail.com>
2015-12-03 18:22:18 -0600

    Add keyframes

--- modal/animations.js
+++ modal/animations.js
@@ -354,17 +354,17 @@ module.exports = {
         }
     },
     '3dSlit': {
-        '@keyframes slit': {
-            '50%': {
-                transform: 'translateZ(-250px) rotateY(89deg)',
-                opacity: 1,
-                'animation-timing-function': 'ease-in'
-            },
-            '100%': {
-                transform: 'translateZ(0) rotateY(0deg)',
-                opacity: 1
-            }
-        },
+        // '@keyframes slit': {
+        //     '50%': {
+        //         transform: 'translateZ(-250px) rotateY(89deg)',
+        //         opacity: 1,
+        //         'animation-timing-function': 'ease-in'
+        //     },
+        //     '100%': {
+        //         transform: 'translateZ(0) rotateY(0deg)',
+        //         opacity: 1
+        //     }
+        // },
         overlay: {
             hidden: {
 
